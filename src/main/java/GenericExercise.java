4cfecfd09d3e3eccc990dba3ba09fda3a130eff0
vtran91 <vtran16@bellsouth.net>
2023-03-03 03:04:25 +0000

    Completed generics lab

--- src/main/java/GenericExercise.java
+++ src/main/java/GenericExercise.java
@@ -21,12 +21,12 @@ public class GenericExercise<T> {
      * @param in an Object that will be of type T at runtime.
      */
     public void loadGenericItem(T in){
-
+        this.data = in ;
     }
     /**
      * @return T data. The type of Data will again be set when the GenericExercise object is created (at runtime).
      */
     public T returnGenericItem(){
-        return null;
+        return this.data;
     }
 }
